61af0125f94ccb5414562e5f62474eb27ed835d1
Aditya Kulkarni <adityapk@gmail.com>
2020-07-28 12:19:27 -0700

    clean dockerfile

--- buildall.sh
+++ buildall.sh
@@ -9,7 +9,7 @@ mkdir -p artifacts/win
 
 # Build for Mac
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    make clean
+    make distclean
     CONFIGURE_FLAGS="--disable-tests --disable-mining --disable-bench" ./zcutil/build.sh -j$(nproc)
     strip src/zcashd
     strip src/zcash-cli
@@ -18,7 +18,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # Build for linux in docker
-docker run --rm -v $(pwd):/opt/zcash adityapk00/zcash:latest bash -c "cd /opt/zcash-linux && git pull && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" ./zcutil/build.sh -j$(nproc) && strip src/zcashd && strip src/zcash-cli && cp src/zcashd src/zcash-cli /opt/zcash/artifacts/linux/"
+docker run --rm -v $(pwd):/opt/zcash adityapk00/zcash:latest bash -c "cd /opt && git clone https://github.com/adityapk00/zcash zcash-linux && cd zcash-linux && git checkout zecwallet-build && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" ./zcutil/build.sh -j$(nproc) && strip src/zcashd && strip src/zcash-cli && cp src/zcashd src/zcash-cli /opt/zcash/artifacts/linux/"
 
 # Build for win in docker
-docker run --rm -v $(pwd):/opt/zcash adityapk00/zcash:latest bash -c "cd /opt/zcash-win && git pull && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" HOST=x86_64-w64-mingw32 ./zcutil/build.sh -j$(nproc) && strip src/zcashd.exe && strip src/zcash-cli.exe && cp src/zcashd.exe src/zcash-cli.exe /opt/zcash/artifacts/win/"
+docker run --rm -v $(pwd):/opt/zcash adityapk00/zcash:latest bash -c "cd /opt && git clone https://github.com/adityapk00/zcash zcash-win && cd zcash-win && git checkout zecwallet-build && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" HOST=x86_64-w64-mingw32 ./zcutil/build.sh -j$(nproc) && strip src/zcashd.exe && strip src/zcash-cli.exe && cp src/zcashd.exe src/zcash-cli.exe /opt/zcash/artifacts/win/"
