be358927b668c4ec09d29654fb623d6cb6b062a9
adityapk00 <aditya@zecwallet.co>
2020-11-15 12:52:13 -0800

    Direct build

--- mkreleaselinux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Building Linux"
-make clean >/dev/null
-./zcutil/build.sh -j$(nproc) >/dev/null
-strip src/zcashd
-strip src/zcash-cli
-cp src/zcashd ../zcash/artifacts/
-cp src/zcash-cli ../zcash/artifacts/
--- mkreleasewin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Building Windows"
-make clean > /dev/null
-HOST=x86_64-w64-mingw32 ./zcutil/build.sh -j$(nproc) >/dev/null
-strip src/zcashd.exe
-strip src/zcash-cli.exe
-cp src/zcashd.exe ../zcash/artifacts/
-cp src/zcash-cli.exe ../zcash/artifacts/
