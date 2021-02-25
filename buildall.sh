6a863507607b30c19ad7ad7f28f40e52da08dcb6
Aditya Kulkarni <zcash@adityapk.com>
2021-02-24 20:36:22 -0800

    Clone depth 1

--- buildall.sh
+++ buildall.sh
@@ -36,16 +36,16 @@ docker build --tag adityapk00/zcash:latest docker
 
 # Build for Mac
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    git clone https://github.com/zcash/zcash
+    git clone --branch $VERSION --depth 1 https://github.com/zcash/zcash
     cd zcash
-    git checkout $VERSION
     ./zcutil/clean.sh
     CONFIGURE_FLAGS="--disable-tests --disable-mining --disable-bench" ./zcutil/build.sh -j$(nproc) && strip src/zcashd && strip src/zcash-cli && cp src/zcashd ../artifacts/mac && cp src/zcash-cli ../artifacts/mac
     cd ..
 fi
 
 # Build for linux in docker using ECC's ubuntu 16.04 image
-docker run --rm -v $(pwd):/opt/zcash electriccoinco/zcashd-build-ubuntu1604 bash -c "cd /opt && git clone https://github.com/zcash/zcash zcash-linux && cd zcash-linux && git checkout $VERSION && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" ./zcutil/build.sh -j$(nproc) && strip src/zcashd && strip src/zcash-cli && cp src/zcashd src/zcash-cli /opt/zcash/artifacts/linux/"
+docker run --rm -v $(pwd):/opt/zcash electriccoinco/zcashd-build-ubuntu1604 bash -c "cd /opt && git clone --branch $VERSION --depth 1 https://github.com/zcash/zcash zcash-linux && cd zcash-linux && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" ./zcutil/build.sh -j$(nproc) && strip src/zcashd && strip src/zcash-cli && cp src/zcashd src/zcash-cli /opt/zcash/artifacts/linux/"
 
 # Build for win in docker, using ECC's debian10 image, which is what they use on their CI as well. 
-docker run --rm -v $(pwd):/opt/zcash adityapk00/zcash:latest bash -c "cd /opt && git clone https://github.com/zcash/zcash zcash-win && cd zcash-win && git checkout $VERSION && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" HOST=x86_64-w64-mingw32 ./zcutil/build.sh -j$(nproc) && strip src/zcashd.exe && strip src/zcash-cli.exe && cp src/zcashd.exe src/zcash-cli.exe /opt/zcash/artifacts/win/"
+docker run --rm -v $(pwd):/opt/zcash adityapk00/zcash:latest bash -c "cd /opt && git clone --branch $VERSION --depth 1 https://github.com/zcash/zcash zcash-win && cd zcash-win && CONFIGURE_FLAGS=\"--disable-tests --disable-mining --disable-bench\" HOST=x86_64-w64-mingw32 ./zcutil/build.sh -j$(nproc) && strip src/zcashd.exe && strip src/zcash-cli.exe && cp src/zcashd.exe src/zcash-cli.exe /opt/zcash/artifacts/win/"
+
